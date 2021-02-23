135e540caea8d6f26c591747a46d7d13f9eec42c
shi-n <shi-n_xyz@cloudsquare.jp>
2021-02-24 06:36:42 +0900

    Initial Commit

---
+++ MusicFilterShuffle/MusicFilterShuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicFilterShuffleApp.swift
+//  MusicFilterShuffle
+//
+//  Created by Shin Inaba on 2021/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicFilterShuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
