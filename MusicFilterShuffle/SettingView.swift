1f633d6403696672a98d1381a66fbe5fc9199b55
shi-n <shi-n_xyz@cloudsquare.jp>
2021-03-01 07:02:53 +0900

    設定画面にForm設定

--- MusicFilterShuffle/SettingView.swift
+++ MusicFilterShuffle/SettingView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        }
     }
 }
 
