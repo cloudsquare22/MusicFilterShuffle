496473410707f38cb6db8a71db48230d682313c9
shi-n <shi-n_xyz@cloudsquare.jp>
2021-03-01 21:18:17 +0900

    Overlay変更。

--- MusicFilterShuffle/FiltersView.swift
+++ MusicFilterShuffle/FiltersView.swift
@@ -10,21 +10,13 @@ import SwiftUI
 struct FiltersView: View {
     @EnvironmentObject var music: Music
     
-    @State var dispProgress = false
     @State var onTap = false
 
     var body: some View {
         NavigationView {
             List {
-                if self.dispProgress == true {
-                    HStack {
-                        Spacer()
-                        ProgressView("selecting music...")
-                        Spacer()
-                    }
-                }
-                FilterView(dispProgress: self.$dispProgress, onTap: self.$onTap, title: "Last Played Date Old", filter: 0)
-                FilterView(dispProgress: self.$dispProgress, onTap: self.$onTap, title: "Play Count Min", filter: 1)
+                FilterView(onTap: self.$onTap, title: "Last Played Date Old", filter: 0)
+                FilterView(onTap: self.$onTap, title: "Play Count Min", filter: 1)
             }
             .padding(8.0)
             .navigationBarTitle("Filters", displayMode: .inline)
@@ -42,33 +34,53 @@ struct FiltersView_Previews: PreviewProvider {
 
 struct FilterView: View {
     @EnvironmentObject var music: Music
+    @State var dispProgress: Bool = false
 
-    @Binding var dispProgress: Bool
     @Binding var onTap: Bool
     let title: String
     let filter: Int
     
     var body: some View {
-        Text(self.title)
-            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            .onTapGesture {
-                if self.onTap == false {
-                    self.onTap = true
-                    self.dispProgress.toggle()
-                    DispatchQueue.global().async {
-                        if self.filter == 0 {
-                            self.music.lastPlayedDateOld()
-                        }
-                        else if self.filter == 1 {
-                            self.music.playCountMin()
-                        }
+        HStack {
+            Text(self.title)
+                .font(.title2)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .onTapGesture {
+                    if self.onTap == false {
+                        self.onTap = true
                         self.dispProgress.toggle()
-                        self.onTap = false
+                        DispatchQueue.global().async {
+                            if self.filter == 0 {
+                                self.music.lastPlayedDateOld()
+                            }
+                            else if self.filter == 1 {
+                                self.music.playCountMin()
+                            }
+                            self.dispProgress.toggle()
+                            self.onTap = false
+                        }
+                    }
+                    else {
+                        print("onTap Noaction!!")
                     }
                 }
-                else {
-                    print("onTap Noaction!!")
-                }
-            }
+            Spacer()
+        }
+        .overlay(OverlayProgressView(dispProgress: self.$dispProgress))
     }
+}
+
+struct OverlayProgressView: View {
+    @Binding var dispProgress: Bool
+
+    var body: some View {
+        if self.dispProgress == true {
+            HStack {
+                Spacer()
+                ProgressView("selecting music...")
+                Spacer()
+            }
+        }
+    }
+
 }
