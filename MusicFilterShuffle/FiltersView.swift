5ac007a4166331fe2dd2c94316db778bc1a78659
shi-n <shi-n_xyz@cloudsquare.jp>
2021-03-06 07:45:26 +0900

    グリッドのデザイン、全体タップボタン試用中

--- MusicFilterShuffle/FiltersView.swift
+++ MusicFilterShuffle/FiltersView.swift
@@ -14,7 +14,7 @@ struct FiltersView: View {
     @State var onTap = false
 
 //    var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
-    var columns: [GridItem] = [GridItem(spacing: 0), GridItem(spacing: 8)]
+    var columns: [GridItem] = [GridItem(spacing: 8), GridItem(spacing: 8)]
 
     var body: some View {
         NavigationView {
@@ -24,6 +24,7 @@ struct FiltersView: View {
                         FilterView(onTap: self.$onTap, title: "Last Played Date Old", filter: 0, size: geometry.size.width)
                         FilterView(onTap: self.$onTap, title: "Play Count Min", filter: 1, size: geometry.size.width)
                     }
+                    .padding(8.0)
                 }
             }
             .navigationBarTitle("Filters", displayMode: .inline)
@@ -61,40 +62,55 @@ struct FilterView: View {
     let size: CGFloat
     
     var body: some View {
-        RoundedRectangle(cornerRadius: 32)
-            .frame(width: CGFloat(size / 2), height: CGFloat(size / 2), alignment: .center)
-            .overlay(
-                Text(self.title)
-                    .font(.title2)
-                    .fontWeight(.light)
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            )
-            .onTapGesture {
-                if self.onTap == false {
-                    self.onTap = true
-                    self.dispProgress.toggle()
-                    DispatchQueue.global().async {
-                        if self.filter == 0 {
-                            self.music.lastPlayedDateOld()
-                        }
-                        else if self.filter == 1 {
-                            self.music.playCountMin(selectMusicCount: self.settingData.selectMusicCount)
-                        }
-                        if self.settingData.autoPlay == true {
-                            self.music.play()
-                        }
-                        self.dispProgress.toggle()
-                        self.onTap = false
-                        self.disapItemsView.toggle()
+        Button(action: {
+            if self.onTap == false {
+                self.onTap = true
+                self.dispProgress.toggle()
+                DispatchQueue.global().async {
+                    if self.filter == 0 {
+                        self.music.lastPlayedDateOld()
                     }
-                }
-                else {
-                    print("onTap Noaction!!")
+                    else if self.filter == 1 {
+                        self.music.playCountMin(selectMusicCount: self.settingData.selectMusicCount)
+                    }
+                    if self.settingData.autoPlay == true {
+                        self.music.play()
+                    }
+                    self.dispProgress.toggle()
+                    self.onTap = false
+                    self.disapItemsView.toggle()
                 }
             }
-            .overlay(OverlayProgressView(dispProgress: self.$dispProgress))
-            .fullScreenCover(isPresented: self.$disapItemsView, onDismiss: {}, content: {
-                ItemsView()
+            else {
+                print("onTap Noaction!!")
+            }
+        }, label: {
+            Text(self.title)
+                .font(.title2)
+                .fontWeight(.light)
+                .frame(width: CGFloat(abs(size / 2 - 8)), height: CGFloat(abs(size / 2 - 8)), alignment: .center)
+//                .background(Color.red)
+                .cornerRadius(32, antialiased: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+        })
+        .overlay(RoundedRectangle(cornerRadius: 32).stroke())
+        .overlay(OverlayProgressView(dispProgress: self.$dispProgress))
+        .fullScreenCover(isPresented: self.$disapItemsView, onDismiss: {}, content: {
+            ItemsView()
+        })
+        Text(self.title)
+            .font(.title2)
+            .fontWeight(.light)
+            .frame(width: CGFloat(abs(size / 2 - 8)), height: CGFloat(abs(size / 2 - 8)), alignment: .center)
+            .onTapGesture {
+                print("aa")
+            }
+            .overlay(RoundedRectangle(cornerRadius: 32).stroke().onTapGesture {
+                print("bb")
+            })
+            .overlay(RoundedRectangle(cornerRadius: 32).foregroundColor(Color.gray.opacity(0.00001)).onTapGesture {
+                print("cc")
+            }.onLongPressGesture {
+                print("cc long")
             })
     }
 }
