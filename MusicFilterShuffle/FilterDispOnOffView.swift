0c4a98cc72fe9ebcc8078a306b14e2c0a856440d
shi-n <shi-n_xyz@cloudsquare.jp>
2021-11-20 21:47:49 +0900

    フィルター表示／非表示設定、チェックボックス追加

--- MusicFilterShuffle/FilterDispOnOffView.swift
+++ MusicFilterShuffle/FilterDispOnOffView.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 struct FilterDispOnOffView: View {
+    @EnvironmentObject var settingData: SettingData
+
     var body: some View {
         List {
             ForEach(0..<FiltersView.filtersData.count) { index in
-                Text(FiltersView.filtersData[index].1.rawValue)
+                HStack {
+                    Text(FiltersView.filtersData[index].1.rawValue)
+                    Spacer()
+                    if self.settingData.filterDispOnOffMap[FiltersView.filtersData[index].1.rawValue] == false {
+                        Image(systemName: "square")
+                    }
+                    else {
+                        Image(systemName: "checkmark.square")
+                    }
+                }
+                .font(.title)
             }
         }
         .navigationTitle("Show/hide filters")
