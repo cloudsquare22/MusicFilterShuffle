b26d006286ff7e8d29dd56553a77c16740fc8e5c
shi-n <shi-n_xyz@cloudsquare.jp>
2024-08-15 19:57:27 +0900

    ライブラリ複数選択画面追加

---
+++ MusicFilterShuffle/LibraryMultiSelectSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryMultiSelectSettingView.swift
+//  MusicFilterShuffle
+//
+//  Created by Shin Inaba on 2024/08/15.
+//
+
+import SwiftUI
+
+struct LibraryMultiSelectSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibraryMultiSelectSettingView()
+}
